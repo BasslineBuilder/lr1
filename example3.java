667b92e354741d23d8734cb794498b0a84aa7010
BasslineBuilder <suppakake@gmail.com>
2025-01-15 20:27:50 +0500

    Add files via upload

--- example3.java
+++ example3.java
@@ -1,15 +1,20 @@
 package labrab_prog_urfu;
 import java.util.Scanner;
 public class example3 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Какой сегодня день: ");
-        int R = in.nextInt();
-        System.out.println("Какой сегодня месяц: ");
-        int E = in.nextInt();
-        System.out.println("Какой сегодня год: ");
-        int W = in.nextInt();
-        System.out.println("Сегодня: "+ R+". "+E+". "+W+" ");
-        in.close();
-    }
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите название текущего дня недели: ");
+        String dayOfWeek = scanner.nextLine();
+        System.out.print("Введите название месяца: ");
+        String month = scanner.nextLine();
+        System.out.print("Введите номер дня в месяце: ");
+        int dayOfMonth = scanner.nextInt();
+        if (dayOfMonth < 1 || dayOfMonth > 31) {
+            System.out.println("Ошибка: номер дня от 1 до 31.");
+        } else {
+            String message = String.format("Сегодня: %s, %d %s", dayOfWeek, dayOfMonth, month);
+            System.out.println(message);
+        }
+        scanner.close();
+	}
 }
